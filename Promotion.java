1e625a29347b3c90283d6b9f3736f245ceec35b2
kkurachova <v.cat.mailbox@gmail.com>
2019-03-01 09:37:09 +0300

    Update Promotion.java

--- Promotion.java
+++ Promotion.java
@@ -1,7 +1,41 @@
 package by.iba.management.model.logic;
 
+
 /**
  * Created by katya on 2/28/2019.
  */
 public class Promotion {
+    private String position = Employee.getPosition();
+    private String level = Employee.getLevel();
+
+    public static void promoteEmployee(String position, String level){
+        switch (position){
+            case "dev":
+                switch (level){
+                    case "junior":
+                        //code
+                        break;
+                    case "middle":
+                        //code
+                        break;
+                    case "senior":
+                        //code
+                        break;
+                }
+                break;
+            case "qa":
+                switch (level){
+                    case "junior":
+                        //code
+                        break;
+                    case "middle":
+                        //code
+                        break;
+                    case "senior":
+                        //code
+                        break;
+                }
+                break;
+        }
+    }
 }

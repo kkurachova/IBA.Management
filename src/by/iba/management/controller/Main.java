ce0010679f21b9eb1fd65ed8fc390b23f4dbd41a
Anastasiya Tuharava <tuharava@gmail.com>
2019-04-18 22:57:38 +0300

    Updated

--- src/by/iba/management/controller/Main.java
+++ src/by/iba/management/controller/Main.java
@@ -4,12 +4,19 @@ import by.iba.management.model.entity.*;
 
 public class Main {
     public static void main(String[] args) {
-        Employee emp1 = new Employee("Nastya", "Tuharava", false, "junior",
+        Employee emp1 = new Employee("Anastasiya", "Tuharava", false, "junior",
                 "A2", new ProgrammingLanguage(true, false, false, false, false),
                 new Skills(true, true, true, true, false),
                 new Testing(false, false,false, false),
                 new Tools(true, true, false, true));
         EmployeesRepository.getEmployeesList().add(emp1);
-        System.out.println(EmployeesRepository.getEmployeesList());
+        Employee emp2 = new Employee("Anna", "Kuznetsova", true, "senior",
+                "B2", new ProgrammingLanguage(true, false, false, false, false),
+                new Skills(true, true, true, true, false),
+                new Testing(false, false,false, false),
+                new Tools(true, true, false, true));
+        EmployeesRepository.getEmployeesList().add(emp2);
+        System.out.println(emp1);
+
     }
 }

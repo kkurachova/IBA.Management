d4a3c2cfbbcd5e4e8cf7c727bd2886997a3e7274
kkurachova <v.cat.mailbox@gmail.com>
2019-04-25 17:00:08 +0300

    Update PositionPattern.java

--- src/by/iba/management/model/entity/PositionPattern.java
+++ src/by/iba/management/model/entity/PositionPattern.java
@@ -9,12 +9,12 @@ import by.iba.management.model.entity.Tools;
  * Created by katya on 3/3/2019.
  */
 public class PositionPattern {
-    Employee qaMiddle = new Employee(false,"junior", "B1", new Testing(true, false, true, true));
+    Employee qaMiddle = new Employee(false,Position.JUNIOR, EnglishLanguageLevel.B1, new Testing(true, false, true, true));
     Employee qaSenior = new Employee(true, "middle", "B2", new Testing(true, true, true, true));
     Employee qaLead = new Employee(true, "senior", "C1", new Testing(true, true, true, true));
 
     Employee devMiddle = new Employee(false, "junior", "B1", new ProgrammingLanguage(true, true, true, false, false), new Skills(true, true, true, false, false), new Tools(true, true, true, false));
-    Employee devSenior = new Employee(true, "middle", "B2", new ProgrammingLanguage(true, true, true, true, false), new Skills(true, true, true, true, false), new Tools(true, true, true, true);
-    Employee devLead = new Employee(true, "senior", "C1", new ProgrammingLanguage(true, true, true, true, true), new Skills(true, true, true, true, true), new Tools(true, true, true, true);
+    Employee devSenior = new Employee(true, "middle", "B2", new ProgrammingLanguage(true, true, true, true, false), new Skills(true, true, true, true, false), new Tools(true, true, true, true));
+    Employee devLead = new Employee(true, "senior", "C1", new ProgrammingLanguage(true, true, true, true, true), new Skills(true, true, true, true, true), new Tools(true, true, true, true));
 
 }

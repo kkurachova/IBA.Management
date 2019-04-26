1418441516f14a74ce5c2061369cb43cbdcfc8f4
kkurachova <v.cat.mailbox@gmail.com>
2019-04-26 22:54:01 +0300

    Update Position.java

--- src/by/iba/management/model/entity/Position.java
+++ src/by/iba/management/model/entity/Position.java
@@ -1,5 +1,5 @@
 package by.iba.management.model.entity;
 
 public enum Position {
-    JUNIOR, MIDDLE, SENIOR
+    Junior_Qa, Middle_Qa, Senior_Qa, Lead_Qa, Junior_Dev, Middle_Dev, Senior_Dev, Lead_Dev
 }

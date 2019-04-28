5ce17b9be65631bb3049f20da3349259190d9983
kkurachova <v.cat.mailbox@gmail.com>
2019-04-29 00:51:31 +0300

    Update EditProject.java

--- src/by/iba/management/model/logic/EditProject.java
+++ src/by/iba/management/model/logic/EditProject.java
@@ -3,63 +3,45 @@ package by.iba.management.model.logic;
 import by.iba.management.model.entity.Project;
 import by.iba.management.model.entity.ProjectsRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 /**
  * Created by katya on 2/20/2019.
  */
-public class EditProject {
-    private static String new_projectName;
-    private static String new_projectDescription;
-    int stackLength = 30;
-    private String[] new_projectStack = new String[stackLength]; // technologies key words for a new team member search functionality
+public class FindProject {
+    private FindProject () {}
 
-    public EditProject(String new_projectName, String new_projectDescription, String[] new_projectStack) {
-        this.new_projectName = new_projectName;
-        this.new_projectDescription = new_projectDescription;
-        this.new_projectStack = new_projectStack;
+    public static List<Project> findProjectById (long projectId) {
+        List<Project> listResult = new ArrayList<>();
+        for (Project p : ProjectsRepository.getProjectList()) {
+            if (p.getProjectId() == projectId) {
+                listResult.add(p);
+            }
+        }
+        return listResult;
     }
 
-
-    public String getNew_projectName() {
-        return new_projectName;
+    public static List<Project> findProject_byProjectName (String projectName) {
+        List<Project> listResult = new ArrayList<>();
+        for (Project p : ProjectsRepository.getProjectList()) {
+            if (p.getProjectName().equals(projectName)) {
+                listResult.add(p);
+            }
+        }
+        return listResult;
     }
 
-    public String getNew_projectDescription() {
-        return new_projectDescription;
+    public static List<Project> findProject_byProjectDescription (String ProjectDescription) {
+        List<Project> listResult = new ArrayList<>();
+        for (Project p : ProjectsRepository.getProjectList()) {
+            if (p.getProjectDescription().equals(ProjectDescription)) {
+                listResult.add(p);
+            }
+        }
+        return listResult;
     }
-
-    public String[] getNew_projectStack() {
-        return new_projectStack;
-    }
-
-    public String editProjectName() {
-        ProjectsRepository.getProjectList().get(Project.getProjectName()).setProjectName(new_projectName);
-        return new_projectName;
-    }
-
-    public String editProjectDescription() {
-        ProjectsRepository.getProjectList().get(Project.getProjectDescription()).setProjectDescription(new_projectDescription);
-        return new_projectDescription;
-    }
-
-    public String[] editProjectName() {
-        ProjectsRepository.getProjectList().get(Project.getProjectStack()).setProjectStack(new_projectStack);
-        return new_projectStack;
-    }
-
-    public void assignProject() {
-
-    }
-
-    public void unassignProject() {
-
-    }
-
-    public void reassignProject() {
-
-    }
-
-    public void removeProject() {
-
-    }
-
 }
+
+

c4b95aaa23521f62eaa0bd9cf5bc819b2600b38b
Anastasiya Tuharava <tuharava@gmail.com>
2019-05-02 17:42:45 +0300

    Updated

--- src/by/iba/management/controller/Main.java
+++ src/by/iba/management/controller/Main.java
@@ -2,10 +2,11 @@ package by.iba.management.controller;
 
 import by.iba.management.util.DataReader;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ArrayList<String> list = DataReader.readFile();
         for (String s : list) {
             System.out.println(s);
--- src/by/iba/management/model/logic/EditProject.java
+++ src/by/iba/management/model/logic/EditProject.java
@@ -1,44 +1,12 @@
 package by.iba.management.model.logic;
-import by.iba.management.model.entity.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import by.iba.management.model.entity.Employee;
+import by.iba.management.model.entity.Project;
 
-/**
- * Created by katya on 2/20/2019.
- */
-
-public class EditProject{
-    public void editProjectName (Project project, String newProjectName) {
-        project.setProjectName(newProjectName);
-    }
-
-    public void editProjectDescription (Project project, String newProjectDescription) {
-        project.setProjectDescription(newProjectDescription);
-    }
-
-    //unassign Employee to a Project from a Project`s page
-    public void unassignEmployee(Employee employee, Project project){
-        project.removeFromTeamList(employee);
-        employee.setProjectId(0);
-    }
-
-    //assign Employee to a Project from a Project`s page
-    public void assignEmployee(Employee employee, Project project) {
-        project.addTeamList(employee);
-        employee.setProjectId(project.getProjectId());
-    }
-
-    public void removeProject(Project project, long projectId) {
-        //remove projectId from all assigned employees:
-        List<Employee> teamList = new ArrayList<>();
-        for (Employee e : EmployeesRepository.getEmployeesList()) {
-            if (e.getProjectId() == projectId) {
-                e.setProjectId(0);
-            }
-        }
-
-        //now remove the project:
-        ProjectsRepository.getProjectList().remove(project);
-    }
+public interface EditProject {
+    void editProjectName(Project project, String newProjectName);
+    void editProjectDescription(Project project, String newProjectDescription);
+    void unassignEmployee(Employee employee, Project project);
+    void assignEmployee(Employee employee, Project project);
+    void removeProject(Project project, long projectId);
 }

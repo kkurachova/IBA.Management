cdd335599bcc3ce141fc9f8d0daf9654a27d726a
kkurachova <v.cat.mailbox@gmail.com>
2019-05-02 10:51:48 +0300

    Update EditProject.java

--- src/by/iba/management/model/logic/EditProject.java
+++ src/by/iba/management/model/logic/EditProject.java
@@ -1,31 +1,44 @@
 package by.iba.management.model.logic;
-
 import by.iba.management.model.entity.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 /**
  * Created by katya on 2/20/2019.
  */
+
 public class EditProject{
     public void editProjectName (Project project, String newProjectName) {
         project.setProjectName(newProjectName);
     }
+
+    public void editProjectDescription (Project project, String newProjectDescription) {
+        project.setProjectDescription(newProjectDescription);
+    }
+
+    //unassign Employee to a Project from a Project`s page
     public void unassignEmployee(Employee employee, Project project){
         project.removeFromTeamList(employee);
         employee.setProjectId(0);
     }
+
     //assign Employee to a Project from a Project`s page
     public void assignEmployee(Employee employee, Project project) {
         project.addTeamList(employee);
         employee.setProjectId(project.getProjectId());
     }
 
-    public void removeProject(Project project) {
+    public void removeProject(Project project, long projectId) {
+        //remove projectId from all assigned employees:
+        List<Employee> teamList = new ArrayList<>();
+        for (Employee e : EmployeesRepository.getEmployeesList()) {
+            if (e.getProjectId() == projectId) {
+                e.setProjectId(0);
+            }
+        }
+
+        //now remove the project:
         ProjectsRepository.getProjectList().remove(project);
     }
 }

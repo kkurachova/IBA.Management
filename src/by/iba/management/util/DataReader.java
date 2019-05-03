300a8b44470b1e62d1b46765201ae7594af54b82
Anastasiya Tuharava <tuharava@gmail.com>
2019-05-03 21:44:20 +0300

    Updated

--- src/by/iba/management/util/DataReader.java
+++ src/by/iba/management/util/DataReader.java
@@ -5,16 +5,11 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
-
-/**
- * Created by katya on 4/26/2019.
- */
 
 public class DataReader {
     private static final String FILE_PATH = "data/EmployeesList.xlsx";

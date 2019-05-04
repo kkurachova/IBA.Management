0d7ed67d9ce917f7c1e9e9d9d1ed70f6e3a87d19
Anastasiya Tuharava <tuharava@gmail.com>
2019-05-04 16:53:56 +0300

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	data/EmployeesList.xlsx

--- src/by/iba/management/controller/Main.java
+++ src/by/iba/management/controller/Main.java
@@ -1,6 +1,7 @@
 package by.iba.management.controller;
 
 import by.iba.management.util.DataReader;
+import by.iba.management.util.DataValidator;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -8,8 +9,10 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) throws IOException {
         ArrayList<String> list = DataReader.readFile();
+        DataValidator validator = new DataValidator();
         for (String s : list) {
             System.out.println(s);
+            System.out.println(validator.validate(s));
         }
     }
 }
--- src/by/iba/management/util/DataValidator.java
+++ src/by/iba/management/util/DataValidator.java
@@ -6,8 +6,9 @@ import java.util.regex.Pattern;
 public class DataValidator {
     private Pattern pattern;
     private Matcher matcher;
-    private static final String REG_PATTERN = "(-?[0-9]*?\\.[0-9]*?\\s){14}(-?[0-9]*?\\.[0-9]*?)";
-
+    private static final String REG_PATTERN = "[0-9]+\\s([a-zA-Z]+\\s){2}[0-9]+\\s(false|true)\\s" +
+            "(JUNIOR_QA|MIDDLE_QA|SENIOR_QA|LEAD_QA|JUNIOR_DEV|MIDDLE_DEV|SENIOR_DEV|LEAD_DEV)\\s(A1|A2|B1|B2|C1|C2)\\s((false|true)\\s){18}";
+    //private static final String REG_PATTERN = "(-?[0-9]*?\\.[0-9]*?\\s){14}(-?[0-9]*?\\.[0-9]*?)";
     public DataValidator() {
         pattern = Pattern.compile(REG_PATTERN);
     }

d5844531a1b750f09892b1616440ceda6c7579a0
kkurachova <v.cat.mailbox@gmail.com>
2019-05-05 16:57:10 +0300

    Update Project.java

--- src/by/iba/management/model/entity/Project.java
+++ src/by/iba/management/model/entity/Project.java
@@ -1,5 +1,7 @@
 package by.iba.management.model.entity;
 
+import by.iba.management.util.ProjectIdGenerator;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
@@ -8,93 +10,58 @@ import java.util.Objects;
  * Created by katya on 2/20/2019.
  */
 public class Project {
-    private int counter = 0;
-    private int projectId;
+    private long projectId;
     private String projectName;
     private String projectDescription;
-    private ArrayList<Employee> teamList = new ArrayList<>();
 
-    public Project(int counter, int projectId, String projectName, String projectDescription,
-                   int stackLength, String[] projectStack) {
-        this.counter = counter;
-        this.projectId = projectId;
+    public Project(long projectId, String projectName, String projectDescription) {
+        this.projectId = ProjectIdGenerator.getProjectId();
         this.projectName = projectName;
         this.projectDescription = projectDescription;
     }
 
-    public Project() {}
-
-    public int getProjectId() {
-        return projectId;
+    public Project() {
     }
 
+    public long getProjectId() {
+        return projectId;
+    }
     public String getProjectName() {
         return projectName;
     }
-
     public String getProjectDescription() {
         return projectDescription;
     }
-
-    public int getCounter() {
-        return counter;
-    }
-
-
-    public void setCounter(int counter) {
-        this.counter = counter;
-    }
-
-    public void setProjectId(int projectId) {
+    public void setProjectId(long projectId) {
         this.projectId = projectId;
     }
-
-
     public void setProjectName(String projectName) {
         this.projectName = projectName;
     }
-
     public void setProjectDescription(String projectDescription) {
         this.projectDescription = projectDescription;
-    }
-
-    public ArrayList<Employee> getTeamList() {
-        return teamList;
-    }
-
-    public void addTeamList(Employee employee) {
-        teamList.add(employee);
-    }
-
-    public void removeFromTeamList(Employee employee) {
-        teamList.remove(employee);
     }
 
     @Override
     public String toString() {
         return "Project{" +
-                "counter=" + counter +
                 ", projectId=" + projectId +
                 ", projectName='" + projectName + '\'' +
                 ", projectDescription='" + projectDescription + '\'' +
                 '}';
     }
-
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Project)) return false;
         Project project = (Project) o;
-        return getCounter() == project.getCounter() &&
-                getProjectId() == project.getProjectId() &&
+        return getProjectId() == project.getProjectId() &&
                 getProjectName().equals(project.getProjectName()) &&
                 Objects.equals(getProjectDescription(), project.getProjectDescription());
     }
-
     @Override
     public int hashCode() {
-        int result = Objects.hash(getCounter(), getProjectId(), getProjectName(), getProjectDescription());
+        int result = Objects.hash(getProjectId(), getProjectName(), getProjectDescription());
         result = 31 * result;
         return result;
     }

44d03b0fc67e8f67ba52f1766d7ca49c284f17d6
kkurachova <v.cat.mailbox@gmail.com>
2019-05-05 17:04:34 +0300

    Update UpdateProjectTeamList.java

--- src/by/iba/management/model/logic/UpdateProjectTeamList.java
+++ src/by/iba/management/model/logic/UpdateProjectTeamList.java
@@ -4,8 +4,6 @@ import by.iba.management.model.entity.Employee;
 import java.util.ArrayList;
 
 public interface UpdateProjectTeamList {
-    ArrayList<Employee> teamList = new ArrayList<>();
-
     void addTeamMember(Employee employee);
     void removeTeamMember(Employee employee);
 }

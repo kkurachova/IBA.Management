ad8f7d92d02e6624ad58206a794fecbdcfcd9abb
kkurachova <v.cat.mailbox@gmail.com>
2019-05-07 18:39:50 +0300

    Update ProjectIdGenerator.java

--- src/by/iba/management/util/ProjectIdGenerator.java
+++ src/by/iba/management/util/ProjectIdGenerator.java
@@ -1,7 +1,7 @@
 package by.iba.management.util;
 
 public class ProjectIdGenerator {
-    private static long ProjectId = 0;
+    private static long ProjectId = 8;
     public static long getProjectId() {
         return ++ProjectId;
     }

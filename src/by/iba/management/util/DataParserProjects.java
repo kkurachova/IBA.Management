6b6dfd2db1368e57e3c880c58c8c694187263e47
kkurachova <v.cat.mailbox@gmail.com>
2019-05-09 16:44:48 +0300

    Add files via upload

--- src/by/iba/management/util/DataParserProjects.java
+++ src/by/iba/management/util/DataParserProjects.java
@@ -4,15 +4,15 @@ import by.iba.management.model.entity.*;
 
 import java.util.ArrayList;
 
-public class DataParser_Projects {
+public class DataParserProjects {
     private static final String REGEX_SPACE = "\\s";
     public static ArrayList<Project> parseStringToCreateProject(ArrayList<String> list) {
         ArrayList<Project> projectsList = new ArrayList<>();
-        DataValidator_Projects validator = new DataValidator_Projects();
+        DataValidatorProjects validator = new DataValidatorProjects();
         for (String str: list) {
             if (validator.validate(str)) {
                 String[] s = str.split(REGEX_SPACE);
-                Project project = new Project(Long.parseLong(s[0]), s[1], s[2]);
+                Project project = new Project(s[0], s[1], s[2]);
                 projectsList.add(project);
             }
         }
--- src/by/iba/management/util/DataValidatorProjects.java
+++ src/by/iba/management/util/DataValidatorProjects.java
@@ -3,11 +3,11 @@ package by.iba.management.util;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class DataValidator_Projects {
+public class DataValidatorProjects {
     private Pattern pattern;
     private Matcher matcher;
     private static final String REG_PATTERN = "[0-9]+\\s([\\p{Alpha}\\p{Digit}\\p{Punct}]+\\s){2}";
-    public DataValidator_Projects() {
+    public DataValidatorProjects() {
         pattern = Pattern.compile(REG_PATTERN);
     }
 
